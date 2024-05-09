c33b7f4e38335a3e2bd825beffe7fa71dd55e30b
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-09 12:34:05 +0200

    Añadido getScreenMode

--- options/displayController.cs
+++ options/displayController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// displayController es una clase que se usa para gestionar la visualización que quiere el jugador.
+/// Puede ser pantalla completa o modo ventana.
+/// </summary>
 public class displayController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Es el modo de visualización de pantalla elegido.
+    /// </summary>
+    private FullScreenMode _selectedScreenMode;
+    /// <summary>
+    /// Método que se ejecuta al inicio del script.
+    /// </summary>
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Método que se ejecuta cada frame para actualizar la lógica.
+    /// </summary>
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Getter que devuelve <see cref="_selectedScreenMode"/>.
+    /// </summary>
+    /// <returns><see cref="_selectedScreenMode"/>.</returns>
+    public FullScreenMode getScreenMode()
+    {
+        return _selectedScreenMode;
     }
 }
