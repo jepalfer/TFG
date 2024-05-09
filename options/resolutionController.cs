c5445a50aac67c03b24469a7243a4034d4be192d
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-09 12:33:14 +0200

    Modificación de setResolution

--- options/resolutionController.cs
+++ options/resolutionController.cs
@@ -9,22 +9,46 @@ using TMPro;
 /// </summary>
 public class resolutionController : MonoBehaviour
 {
+    /// <summary>
+    /// Lista con todas las resoluciones posibles para la pantalla del jugador.
+    /// </summary>
     private Resolution[] _resolutions;
+
+    /// <summary>
+    /// Lista con las resoluciones posibles para la tasa de refresco del jugador.
+    /// </summary>
     private List<Resolution> _filteredResolutions;
 
+    /// <summary>
+    /// Tasa de refresco a la cual está jugando el jugador.
+    /// </summary>
     private int _currentRefreshRate;
+
+    /// <summary>
+    /// Índice en la lista <see cref="_filteredResolutions"/> de la resolución a la que está jugando el jugador.
+    /// </summary>
     private int _currentResolutionIndex = 0;
+
+    /// <summary>
+    /// Dropdown para visualizar la lista de resoluciones.
+    /// </summary>
     [SerializeField] private TMP_Dropdown _resolutionDropdown;
-    // Start is called before the first frame update
+
+    /// <summary>
+    /// Método que se ejecuta al inicio del script.
+    /// </summary>
     void Start()
     {
+        //Obtenemos todas las resoluciones
         _resolutions = Screen.resolutions;
         _filteredResolutions = new List<Resolution>();
 
-
         _resolutionDropdown.ClearOptions();
+
+        //Obtenemos la tasa de refresco a la que está la pantalla
         _currentRefreshRate = Screen.currentResolution.refreshRate;
 
+        //Obtenemos las resoluciones con los mismos Hz
         for (int i = 0; i < _resolutions.Length; ++i)
         {
             if (_resolutions[i].refreshRate == _currentRefreshRate)
@@ -33,6 +57,7 @@ public class resolutionController : MonoBehaviour
             }
         }
 
+        //Creamos las opciones
         List<string> options = new List<string>();
 
         for (int i = 0; i < _filteredResolutions.Count; ++i)
@@ -45,17 +70,26 @@ public class resolutionController : MonoBehaviour
             }
         }
 
+        //Creamos el dropdown
         _resolutionDropdown.AddOptions(options);
         _resolutionDropdown.value = _currentResolutionIndex;
         _resolutionDropdown.RefreshShownValue();
     }
 
+    /// <summary>
+    /// Setter que modifica la resolución de la pantalla.
+    /// </summary>
+    /// <param name="resolutionIndex">Índice de la resolución seleccionada en la lista.</param>
     public void setResolution(int resolutionIndex)
     {
         Resolution res = _filteredResolutions[resolutionIndex];
-        Screen.SetResolution(res.width, res.height, true);
+        Screen.SetResolution(res.width, res.height, GetComponent<displayController>().getScreenMode());
     }
 
+    /// <summary>
+    /// Getter que devuelve <see cref="_resolutionDropdown"/>.
+    /// </summary>
+    /// <returns><see cref="_resolutionDropdown"/>.</returns>
     public TMP_Dropdown getResolutionDropdown()
     {
         return _resolutionDropdown;
