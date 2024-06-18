b4720734489ca0fd14ae5ddab2797c81580a5aa4
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-06-19 01:01:36 +0200

    Cambios menores

--- skillTrees/skillSlotLogic.cs
+++ skillTrees/skillSlotLogic.cs
@@ -15,11 +15,26 @@ public class skillSlotLogic : MonoBehaviour
     [SerializeField] private Image _skillSprite;
 
     /// <summary>
+    /// Referencia a la imagen de fondo de la habilidad.
+    /// </summary>
+    [SerializeField] private Image _backgroundImage;
+
+    /// <summary>
     /// Setter que modifica el sprite de <see cref="_skillSprite"/>.
     /// </summary>
     /// <param name="sprite">Sprite de la habilidad.</param>
     public void setSkillSprite(Sprite sprite)
     {
         _skillSprite.sprite = sprite;
+    }
+
+
+    /// <summary>
+    /// Setter que modifica el sprite de <see cref="_backgroundImage"/>.
+    /// </summary>
+    /// <param name="color">Sprite del color.</param>
+    public void setBackgroundColor(Sprite color)
+    {
+        _backgroundImage.sprite = color;
     }
 }
